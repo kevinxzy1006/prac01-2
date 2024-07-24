e964702330766608a1b8eb7ccab894ce427c18d9
kevinxzy1006 <161264609+kevinxzy1006@users.noreply.github.com>
2024-07-24 13:10:18 +0930

    main-1-4.cpp

--- main-1-4.cpp
+++ main-1-4.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
-#include "function-1-4.cpp"
+
+using namespace std;
 
 int main() {
-    int array[5] = {1,2,3,4,5};
-    int secondarray[5] = {6,7,8,9,0};
-    int n = sizeof(array)/sizeof(array[0]);
-    int secondn = sizeof(secondarray)/sizeof(secondarray[0]);
-    if(n == secondn) {
-        int sumarray = sum_two_arrays(array, secondarray, n);
-    } else {
-        std::cout << "Error: The size of two array are not equal." << std::endl;
-    }
+    int array1[] = {1, 2, 3, 4, 5};
+    int array2[] = {6, 7, 8, 9, 10};
+    int n = sizeof(array1) / sizeof(array1[0]);
+    
+    int result = sum_two_arrays(array1, array2, n);
+    cout << "Sum of two arrays: " << result << endl;
+
     return 0;
 }
