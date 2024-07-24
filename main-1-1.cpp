d6d3b695a1cefa102b74523e1897c926a7c24067
kevinxzy1006 <161264609+kevinxzy1006@users.noreply.github.com>
2024-07-24 13:02:55 +0930

    main-1-1.cpp

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -6,8 +6,7 @@ using namespace std;
 int main() {
     int array[] = {1, 2, 3, 4, 5};
     int n = sizeof(array)/ sizeof(array[0]);
-    int sum = array_sum(array, n);
-    cout << sum << endl;
+    cout << array_sum(array, n) << endl;
 
     return 0;
 }
