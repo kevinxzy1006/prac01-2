8ca68a2fa29df1c86893af3568eabce43fd428cd
kevinxzy1006 <161264609+kevinxzy1006@users.noreply.github.com>
2024-07-24 13:23:42 +0930

    function-3-4.cpp

--- function-3-4.cpp
+++ function-3-4.cpp
@@ -1,27 +1,16 @@
-#include <iostream>
-using namespace std;
-
 void print_pass_fail(char grade) {
     switch (grade) {
         case 'A':
-            cout << "Pass" << endl;
-            break;
         case 'B':
-            cout << 'Pass' << endl;
-            break;
         case 'C':
-            cout << "Pass" << endl;
+            std::cout << "Pass\n";
             break;
         case 'D':
-            cout << 'Fail' << endl;
-            break;
         case 'E':
-            cout << 'Fail' << endl;
+            std::cout << "Fail\n";
+            break;
+        default:
+            std::cout << "Nothing\n";
             break;
     }
-}
-
-int main() {
-    char grade = 'E';
-    print_pass_fail(grade);
 }
