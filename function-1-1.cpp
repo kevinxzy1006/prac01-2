98405456780115a5ad37a09a02241628d3f41409
kevinxzy1006 <161264609+kevinxzy1006@users.noreply.github.com>
2024-07-24 13:05:26 +0930

    Update function-1-1.cpp

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -1,15 +1,7 @@
-#include <iostream>
-
 int array_sum(int array[], int n) {
     int sum = 0;
-    
-    if(n < 1) {
-        return 0;
-    }
-
-    for (int i=0; i<n; i++) {
-        sum = sum + array[i];
+    for (int i = 0; i < n; i++) {
+        sum += array[i];
     }
     return sum;
 }
-
