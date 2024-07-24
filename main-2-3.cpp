763a5834342e87f69b3b82c1c9e3b8fb2d3786f6
kevinxzy1006 <161264609+kevinxzy1006@users.noreply.github.com>
2024-07-24 13:27:00 +0930

    main-2-3.cpp

--- main-2-3.cpp
+++ main-2-3.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include "function-2-3.cpp"
 
+using namespace std;
+
 int main() {
-    int array[] = {2, 5, 9, 2, 9, 9, 5, 2, 9, 9, 9, 9, 9};
-    int n = sizeof(array) / sizeof(array[0]);
-    
+    int array[10] = {2,2,5,5,9,9,2,5,8,0};
+    int n = sizeof(array)/sizeof(array[0]);
     two_five_nine(array, n);
 
     return 0;
