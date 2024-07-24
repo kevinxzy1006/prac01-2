4481c3b52d41176924055465a13d3bbc207c5cb1
kevinxzy1006 <161264609+kevinxzy1006@users.noreply.github.com>
2024-07-24 13:19:41 +0930

    function-3-1.cpp

--- function-3-1.cpp
+++ function-3-1.cpp
@@ -1,15 +1,21 @@
-#include <iostream>
-
 bool is_fanarray(int array[], int n) {
-    if(n<1) {
+    if (n < 1) {
         return false;
-    } else {
-        for(int i=0; i<n; i++) {
-            if(array[i] = array[n-i]) {
-                return true;
-            } else {
-                return false;
-            }
+    }
+
+    // Check if the array is a palindrome
+    for (int i = 0; i < n / 2; ++i) {
+        if (array[i] != array[n - i - 1]) {
+            return false;
         }
     }
+
+    // Check if the array is in ascending order from the start to the middle element
+    for (int i = 0; i < n / 2 - 1; ++i) {
+        if (array[i] > array[i + 1]) {
+            return false;
+        }
+    }
+
+    return true;
 }
