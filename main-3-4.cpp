ec21c1fa4be68ff986c464f9101eb396793a6a24
kevinxzy1006 <161264609+kevinxzy1006@users.noreply.github.com>
2024-07-24 13:28:38 +0930

    main-3-4.cpp

--- main-3-4.cpp
+++ main-3-4.cpp
@@ -1,14 +1,9 @@
 #include <iostream>
 #include "function-3-4.cpp"
 
+using namespace std;
+
 int main() {
-    char grades[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G'};
-    int n = sizeof(grades) / sizeof(grades[0]);
-
-    for (int i = 0; i < n; ++i) {
-        std::cout << "Grade " << grades[i] << ": ";
-        print_pass_fail(grades[i]);
-    }
-
-    return 0;
+    char grade = 'E';
+    print_pass_fail(grade);
 }
