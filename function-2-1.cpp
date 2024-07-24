f59be653f21f8ea2962a4f4a5b33ad34fce16c23
kevinxzy1006 <161264609+kevinxzy1006@users.noreply.github.com>
2024-07-24 13:13:33 +0930

    function-2-1.cpp

--- function-2-1.cpp
+++ function-2-1.cpp
@@ -1,11 +1,14 @@
-#include <iostream>
-
 int min_element(int array[], int n) {
-    int min = 0;
-    for(int i = 0; i < n; i++) {
-        if(array[i] > array[i+1]) {
-            min = array[i+1];
+    if (n < 1) {
+        return 0;
+    }
+
+    int min = array[0];
+    for (int i = 1; i < n; ++i) {
+        if (array[i] < min) {
+            min = array[i];
         }
     }
+    
     return min;
 }
