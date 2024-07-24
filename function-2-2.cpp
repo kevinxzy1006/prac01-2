c3ccf6d015868ddd2ad0de7ee69f9b02737c04e9
kevinxzy1006 <161264609+kevinxzy1006@users.noreply.github.com>
2024-07-24 13:15:52 +0930

    function-2-2.cpp

--- function-2-2.cpp
+++ function-2-2.cpp
@@ -1,11 +1,14 @@
-#include <iostream>
-
 int max_element(int array[], int n) {
-    int max = 0;
-    for(int i = 0; i < n; i++) {
-        if(array[i] > array[i+1]) {
+    if (n < 1) {
+        return 0;
+    }
+
+    int max = array[0];
+    for (int i = 1; i < n; ++i) {
+        if (array[i] > max) {
             max = array[i];
         }
     }
+    
     return max;
 }
