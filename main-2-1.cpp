8c4489b8540580e4f83a6dd23bbbafb29aa4cd9d
kevinxzy1006 <161264609+kevinxzy1006@users.noreply.github.com>
2024-07-25 17:56:04 +0930

    main-2-1.cpp

--- main-2-1.cpp
+++ main-2-1.cpp
@@ -1,14 +1,10 @@
 #include <iostream>
-#include "function-2-1.cpp"
-
-using namespace std;
+extern int min_element(int array[], int n);
 
 int main() {
-    int array[] = {5, 3, 8, 1, 2};
-    int n = sizeof(array) / sizeof(array[0]);
-    
-    int result = min_element(array, n);
-    cout << "Minimum element: " << result << endl;
+    int array[7] = {1,2,3,4,5,6,7};
+    int n = sizeof(array)/sizeof(array[0]);
+    int min_value = min_element(array, n);
 
     return 0;
 }
