f8a0839daa78c5e1d204f2c421ceadfd3c3e2669
kevinxzy1006 <161264609+kevinxzy1006@users.noreply.github.com>
2024-07-25 18:18:16 +0930

    main-3-1.cpp

--- main-3-1.cpp
+++ main-3-1.cpp
@@ -2,21 +2,9 @@
 extern bool is_fanarray(int array[], int n);
 
 int main() {
-    int array1[] = {1, 2, 3, 2, 1};
-    int n1 = sizeof(array1) / sizeof(array1[0]);
-    std::cout << "Array {1, 2, 3, 2, 1} is a fan array: " << is_fanarray(array1, n1) << std::endl;
-
-    int array2[] = {2, 4, 4, 2};
-    int n2 = sizeof(array2) / sizeof(array2[0]);
-    std::cout << "Array {2, 4, 4, 2} is a fan array: " << is_fanarray(array2, n2) << std::endl;
-
-    int array3[] = {1, 2, 1, 2, 1};
-    int n3 = sizeof(array3) / sizeof(array3[0]);
-    std::cout << "Array {1, 2, 1, 2, 1} is a fan array: " << is_fanarray(array3, n3) << std::endl;
-
-    int array4[] = {1, 3, 5, 4, 2};
-    int n4 = sizeof(array4) / sizeof(array4[0]);
-    std::cout << "Array {1, 3, 5, 4, 2} is a fan array: " << is_fanarray(array4, n4) << std::endl;
+    int array[9] = {1,2,3,4,5,6,7,8,9};
+    int n = sizeof(array)/sizeof(array[0]);
+    is_fanarray(array, n);
 
     return 0;
 }
