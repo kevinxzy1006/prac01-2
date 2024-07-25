52c5079fdc712720f42aa75bdfee5a7b43d47487
kevinxzy1006 <161264609+kevinxzy1006@users.noreply.github.com>
2024-07-25 17:16:41 +0930

    function-2-3.cpp

--- function-2-3.cpp
+++ function-2-3.cpp
@@ -3,17 +3,27 @@
 using namespace std;
 
 void two_five_nine(int array[], int n) {
+    //produce a line of output if the size parameter, n, is less than 1.
+    if(n < 1) {
+        cout << "Error: Size of parameter is too low." << endl;
+    }
+
     //Use uppercase letters to represent the times of appearance of 2, 5 and 9
     int T = 0;
     int F = 0;
     int N = 0;
-    for(int i = 0; i < n; i++) {
-        if(array[i] == 2) {
-            T++;
-        } else if(array[i == 5]) {
-            F++;
-        } else if(array[i == 9]) {
-            N++;
+    for(int i=0; i<n; i++) {
+        //use switch statement to record the occurrance of the numbers
+        switch(array[i]) {
+            case 2:
+                T++;
+                break;
+            case 5:
+                F++;
+                break;
+            case 9:
+                N++;
+                break;
         }
     }
     //output result
